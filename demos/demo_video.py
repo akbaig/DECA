fdb6380065dfe5e075523126f5a4b80dd7175f8a
akbaig <ahmadkamalbaig@gmail.com>
2023-12-28 00:13:35 +0200

    added demo_video to handle video stream, compatible with voca

---
+++ demos/demo_video.py
@@ -0,0 +1,117 @@
+# -*- coding: utf-8 -*-
+#
+# Max-Planck-Gesellschaft zur Förderung der Wissenschaften e.V. (MPG) is
+# holder of all proprietary rights on this computer program.
+# Using this computer program means that you agree to the terms 
+# in the LICENSE file included with this software distribution. 
+# Any use not explicitly granted by the LICENSE is prohibited.
+#
+# Copyright©2019 Max-Planck-Gesellschaft zur Förderung
+# der Wissenschaften e.V. (MPG). acting on behalf of its Max Planck Institute
+# for Intelligent Systems. All rights reserved.
+#
+# For comments or questions, please email us at deca@tue.mpg.de
+# For commercial licensing contact, please contact ps-license@tuebingen.mpg.de
+
+# Format
+# python demos/demo_video.py -i TestSamples/stream --savefolder TestSamples/stream/results --rasterizer_type=pytorch3d -a audio/stream.wav
+
+
+import os, sys
+import cv2
+import numpy as np
+from time import time
+from scipy.io import savemat
+import argparse
+from tqdm import tqdm
+import torch
+from psbody.mesh import Mesh
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from decalib.deca import DECA
+from decalib.datasets import datasets 
+from decalib.utils import util
+from decalib.utils import voca_util
+from decalib.utils.config import cfg as deca_cfg
+from decalib.utils.tensor_cropper import transform_points
+
+def main(args):
+    # if args.rasterizer_type != 'standard':
+    #     args.render_orig = False
+    savefolder = args.savefolder
+    device = args.device
+    os.makedirs(savefolder, exist_ok=True)
+
+    # load test images 
+    testdata = datasets.TestData(args.inputpath, iscrop=args.iscrop, face_detector=args.detector)
+
+    sequence_vertices = np.empty((0, 5023, 3))
+    sequence_trans_vertices = np.empty((0, 5023, 3))
+
+    # run DECA
+    deca_cfg.model.use_tex = args.useTex
+    deca_cfg.rasterizer_type = args.rasterizer_type
+    deca = DECA(config = deca_cfg, device=device)
+    # for i in range(len(testdata)):
+    for i in tqdm(range(len(testdata))):
+        name = testdata[i]['imagename']
+        images = testdata[i]['image'].to(device)[None,...]
+        with torch.no_grad():
+            codedict = deca.encode(images)
+            opdict, visdict = deca.decode(codedict) #tensor
+            sequence_vertices = np.append(sequence_vertices, opdict["verts"].cpu().numpy(), axis=0)
+            sequence_trans_vertices = np.append(sequence_trans_vertices, opdict["trans_verts"].cpu().numpy(), axis=0)
+
+    print(sequence_vertices.shape)
+    print(sequence_trans_vertices.shape)
+    template_mesh = Mesh(filename='data/FLAME_sample.ply')
+    voca_util.output_sequence_meshes(sequence_vertices, template_mesh, savefolder)
+    if args.visualize:
+        voca_util.render_sequence_meshes(args.audiopath, sequence_vertices, template_mesh, savefolder)
+        
+        
+if __name__ == '__main__':
+
+
+
+    parser = argparse.ArgumentParser(description='DECA: Detailed Expression Capture and Animation')
+
+    parser.add_argument('-i', '--inputpath', default='TestSamples/examples', type=str,
+                        help='path to the test data, can be image folder, image path, image list, video')
+    parser.add_argument('-a', '--audiopath', default='audio/example.wav', type=str,
+                        help='path to the audio file')
+    parser.add_argument('-s', '--savefolder', default='TestSamples/examples/results', type=str,
+                        help='path to the output directory, where results(obj, txt files) will be stored.')
+    parser.add_argument('--device', default='cuda', type=str,
+                        help='set device, cpu for using cpu' )
+    # process test images
+    parser.add_argument('--iscrop', default=True, type=lambda x: x.lower() in ['true', '1'],
+                        help='whether to crop input image, set false only when the test image are well cropped' )
+    parser.add_argument('--detector', default='fan', type=str,
+                        help='detector for cropping face, check decalib/detectors.py for details' )
+    # rendering option
+    parser.add_argument('--rasterizer_type', default='standard', type=str,
+                        help='rasterizer type: pytorch3d or standard' )
+    parser.add_argument('--render_orig', default=True, type=lambda x: x.lower() in ['true', '1'],
+                        help='whether to render results in original image size, currently only works when rasterizer_type=standard')
+    parser.add_argument('--visualize', default=False, type=lambda x: x.lower() in ['true', '1'],
+                        help='whether to output video' )
+
+    # save
+    parser.add_argument('--useTex', default=False, type=lambda x: x.lower() in ['true', '1'],
+                        help='whether to use FLAME texture model to generate uv texture map, \
+                            set it to True only if you downloaded texture model' )
+    parser.add_argument('--saveVis', default=True, type=lambda x: x.lower() in ['true', '1'],
+                        help='whether to save visualization of output' )
+    parser.add_argument('--saveKpt', default=False, type=lambda x: x.lower() in ['true', '1'],
+                        help='whether to save 2D and 3D keypoints' )
+    parser.add_argument('--saveDepth', default=False, type=lambda x: x.lower() in ['true', '1'],
+                        help='whether to save depth image' )
+    parser.add_argument('--saveObj', default=False, type=lambda x: x.lower() in ['true', '1'],
+                        help='whether to save outputs as .obj, detail mesh will end with _detail.obj. \
+                            Note that saving objs could be slow' )
+    parser.add_argument('--saveMat', default=False, type=lambda x: x.lower() in ['true', '1'],
+                        help='whether to save outputs as .mat' )
+    parser.add_argument('--saveImages', default=False, type=lambda x: x.lower() in ['true', '1'],
+                        help='whether to save visualization output as seperate images' )
+    main(parser.parse_args())
